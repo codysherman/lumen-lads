022f0ebd2e6315c0c5510b09108038230fa26d73
Cody Sherman <cody@codysherman.com>
2021-02-05 00:12:33 -0600

    remove custom path for url

--- vue.config.js
+++ vue.config.js
@@ -15,7 +15,4 @@ module.exports = {
       .loader('vue-svg-loader')
       .options({ svgo: { plugins: [ { removeDimensions: true }, { removeViewBox: false } ] } });
   },
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/lumen-lads/'
-    : '/',
 };
