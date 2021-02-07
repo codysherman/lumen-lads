92ba067a2394e00c9f9e3913c59110df1fc34fc4
Tom <poci14@gmail.com>
2021-02-07 14:14:08 -0600

    basic helloworld page for profile page

---
+++ src/components/pages/Profile.vue
@@ -0,0 +1,26 @@
+<style scoped lang="sass">
+
+</style>
+
+<template lang="pug">
+div
+  div
+    | hello
+</template>
+
+<script>
+export default {
+  name: 'Profile',
+  metaInfo() {
+    return {
+      title:
+      'Lumen Lads',
+    };
+  },
+  components: { },
+  data() {
+    return { };
+  },
+  methods: {},
+};
+</script>
