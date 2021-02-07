737417decc9939352f6a756328a389ae2a099c3a
Tom <poci14@gmail.com>
2021-02-07 15:51:05 -0600

    added basic signup page

---
+++ src/components/pages/SignUp.vue
@@ -0,0 +1,57 @@
+<style scoped lang="sass">
+
+</style>
+
+<template lang="pug">
+div
+  form(@submit.prevent="submitForm")
+    label(
+      for="userName"
+    )
+      | User Name
+    input(
+      type="text"
+      id="userName"
+      name="userName"
+      v-model="userName"
+    )
+    label(
+      for="userPassword"
+    )
+      | Password
+    input(
+      type="password"
+      id="userPassword"
+      name="userPassword"
+      v-model="userPassword"
+    )
+    button(
+      type="submit"
+    )
+      | Submit
+
+</template>
+
+<script>
+export default {
+  name: 'Profile',
+  metaInfo() {
+    return {
+      title:
+      'Lumen Lads',
+    };
+  },
+  components: { },
+  data() {
+    return {
+      userName: '',
+      userPassword: '',
+    };
+  },
+  methods: {
+    submitForm() {
+      console.log('hello', this.userName, this.userPassword);
+    },
+  },
+};
+</script>
