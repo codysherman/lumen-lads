0ed486164d6d758016608e67161b3579ac889546
Tom <poci14@gmail.com>
2021-02-07 14:15:03 -0600

    added profile page to router

--- src/router.js
+++ src/router.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
 
+import Profile from '@/components/pages/Profile';
+
 Vue.use(Router);
 
 export default new Router({
@@ -12,6 +14,11 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home,
+    },
+    {
+      path: '/profile',
+      name: 'Profile',
+      component: Profile,
     },
   ],
 });
