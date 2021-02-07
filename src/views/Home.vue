901c407300804483b3be337d27dba23ea412728f
Tom <poci14@gmail.com>
2021-02-07 13:35:24 -0600

    display list of users

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <style scoped lang="sass">
-
+  .user-list:hover
+    background-color: red 
 </style>
 
 <template lang="pug">
 div
   User
-  div
-    | {{users}}
+  div(v-for="user in users")
+    .user-list {{ user.name }}
 </template>
 
 <script>
-import {Users} from '@/utils/enums/Users.js'
+import { Users } from '@/utils/enums/Users.js'
 
-import User from '@/components/User'
+import User from '@/components/User.vue'
 
 export default {
   name: 'Home',
